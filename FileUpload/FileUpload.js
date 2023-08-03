3b3526ec5c8e80dc0cd81694f35bc23ab8c5b213
moinakhtar <moinakhtar@hotmail.co.uk>
2023-08-03 14:08:09 +0100

    Update FileUpload.js

--- FileUpload/FileUpload.js
+++ FileUpload/FileUpload.js
@@ -1,7 +1,31 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
-    `<input type="file" id="fileInput"><button type="button" onclick="handleFile()" id="btnFileUpload">Upload</button><script>function handleFile() {
+    `<input type="file" id="fileInput"><button type="button" onclick="handleFile()" id="btnFileUpload">Upload</button>` ;   
+   
+    class FileUploadClass extends HTMLElement {
+        constructor() {
+            super();
+            this.init();           
+        }
+
+        init() {            
+              
+            let shadowRoot = this.attachShadow({mode: "open"});
+            shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            this.addEventListener("click", event => {
+            var event = new Event("onClick");
+            this.fireChanged();           
+            this.dispatchEvent(event);
+            });           
+        }
+        fireChanged() {
+            console.log("OnClick Triggered - checkpoint Moin");  
+            //write logic here to handleFile
+            //alert("show alert - "+document.getElementById('btnFileUpload').type);
+            
+        }    
+        function handleFile() {
   const fileInput = document.getElementById('fileInput');
   const file = fileInput.files[0];
   if (file) {
@@ -32,33 +56,9 @@ function parseFileContent(fileContent) {
   }
   return dataArray;
 }
-</script>` ;   
-   
-    class PerformanceHelp extends HTMLElement {
-        constructor() {
-            super();
-            this.init();           
-        }
-
-        init() {            
-              
-            let shadowRoot = this.attachShadow({mode: "open"});
-            shadowRoot.appendChild(tmpl.content.cloneNode(true));
-            this.addEventListener("click", event => {
-            var event = new Event("onClick");
-            this.fireChanged();           
-            this.dispatchEvent(event);
-            });           
-        }
-
-        fireChanged() {
-            console.log("OnClick Triggered - checkpoint Moin");  
-            //write logic here to handleFile
-            //alert("show alert - "+document.getElementById('btnFileUpload').type);
-            
-        }        
         
     }
 
-    customElements.define('custom-button', PerformanceHelp);
+    customElements.define('file-upload', FileUploadClass);
+    
 })();
