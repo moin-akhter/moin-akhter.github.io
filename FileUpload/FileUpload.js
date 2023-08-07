ddedb8fd0eefcab68eeb07e376b827d1997f785d
moinakhtar <moinakhtar@hotmail.co.uk>
2023-08-07 11:30:48 +0100

    Update FileUpload.js

--- FileUpload/FileUpload.js
+++ FileUpload/FileUpload.js
@@ -1,26 +1,31 @@
 (function () {
     let tmpl = document.createElement('template');
     tmpl.innerHTML = 
-    `<input type="file" id="fileInput"><button type="button" id="btnFileUpload">Upload</button>` ;   
-    
-    class FileUploadClass extends HTMLElement {
+    `<button type="button" id="myBtn">Helper Button</button>` ;   
+   
+    class PerformanceHelp extends HTMLElement {
         constructor() {
             super();
             this.init();           
         }
 
-        init() {                       
+        init() {            
+              
             let shadowRoot = this.attachShadow({mode: "open"});
             shadowRoot.appendChild(tmpl.content.cloneNode(true));
             this.addEventListener("click", event => {
             var event = new Event("onClick");
-            this.fireChanged();            
+            this.fireChanged();           
             this.dispatchEvent(event);
-            });          
-        } 
+            });           
+        }
+
         fireChanged() {
-            console.log("OnClick Triggered - checkpoint Moin");  
-        }  
-    customElements.define('file-upload', FileUploadClass);
-    
+            console.log("OnClick Triggered");     
+            
+        }        
+        
+    }
+
+    customElements.define('custom-button', PerformanceHelp);
 })();
